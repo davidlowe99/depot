d8c632d04dd2f7ddbdd98f9ff52fedab0ea17384
davidlowe99 <dave.lowe@outlook.com>
2013-11-21 14:46:20 -0600

    Finished Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    #Just for fun
+    @count = increment_count
+    #Just for fun
   end
+  
+  #Just for fun
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+  end
+  #Just for fun
 end
